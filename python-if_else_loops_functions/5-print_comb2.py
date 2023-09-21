daecfe632a173d823aed85f13d13ad1b1423468e
MarcosBremermann <mabm2004@gmail.com>
2023-09-21 17:24:57 +0000

    Task 5 v1.01: removed a print command

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 for i in range(00, 100):
-    if i < 10:
-        print("0{}, ".format(i), end="")
-        continue
-
-    elif 10 <= i < 99:
-        print("{}, ".format(i), end="")
+    if i < 99:
+        print("{:02d}, ".format(i), end="")
         continue
 
     elif i >= 99:
