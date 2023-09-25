3dd11ea833a44b61a32435bc8305b8314aa19bfe
MarcosBremermann <mabm2004@gmail.com>
2023-09-25 18:55:02 +0000

    Task 0 v1.0

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    fir_row = []
+
+    for i in matrix:
+        sec_row = []
+
+        for x in i:
+            sec_row.append(x ** 2)
+
+        fir_row.append(sec_row)
+    
+    return fir_row
