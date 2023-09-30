550dec229bf627036735ee0647232e0ce2551439
MarcosBremermann <mabm2004@gmail.com>
2023-09-30 00:27:02 +0000

    Task 6 v1.0

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    b_dictionary = list(a_dictionary)
+    b_dictionary.sort()
+    c_dictionary = {i: a_dictionary[i] for i in b_dictionary}
+    print (c_dictionary)
