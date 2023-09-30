2c6bde31e41f8706f8bcf2b5265bd61749ea13d2
MarcosBremermann <mabm2004@gmail.com>
2023-09-30 00:35:49 +0000

    Task 6 v1.02: fixed the code

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,5 @@ def print_sorted_dictionary(a_dictionary):
     b_dictionary = list(a_dictionary)
     b_dictionary.sort()
     c_dictionary = {i: a_dictionary[i] for i in b_dictionary}
-    print(c_dictionary)
+    for key, value in c_dictionary.items():
+        print("{}: {}".format(key, value))
