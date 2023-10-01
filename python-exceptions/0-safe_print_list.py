e903bb8d5b5bf3e5166f151710af2e785a610ee8
MarcosBremermann <mabm2004@gmail.com>
2023-10-01 05:20:50 +0000

    Task 0 v1.01: fixed output

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     for i in range(x):
         if i < len(my_list):
-            end = "" if i < x - 1 else "\n"
+            end = "" if i < len(my_list) - 1 else "\n"
             print("{}".format(my_list[i]), end=end)
             count += 1
     return count
