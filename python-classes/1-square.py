06859a4f5d5b820192482836f8dfb2acc0a3bfe7
MarcosBremermann <mabm2004@gmail.com>
2023-10-04 05:50:32 +0000

    Task 1 v1.0

---
+++ python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+File that defines a class Square
+"""
+
+
+class Square:
+    """
+    class Square
+    """
+
+    def __init__(self, size):
+        """
+        This is an initialiation that defines size as a
+        Private instance attribute
+        """
+
+        self.__size = size
