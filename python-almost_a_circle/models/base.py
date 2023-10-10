49ba60f2ed0171c067cc996e44465c5d25bd787e
MarcosBremermann <mabm2004@gmail.com>
2023-10-10 17:59:28 +0000

    Task 1 v1.0

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+base.py - Defines the Base class, the foundation\
+for all other classes in the project.
+"""
+
+
+class Base:
+    """
+    Class-level attribute to keep track of\
+    the number of instances created.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Constructor for the Base class
+        """
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
