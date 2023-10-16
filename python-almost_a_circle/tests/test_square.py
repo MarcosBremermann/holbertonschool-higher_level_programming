4a9ea95d2432fada0acd99a7495f5073d9a586d6
MarcosBremermann <mabm2004@gmail.com>
2023-10-16 00:13:53 +0000

    Tests fixes

--- python-almost_a_circle/tests/test_square.py
+++ python-almost_a_circle/tests/test_square.py
@@ -1,68 +1,32 @@
 #!/usr/bin/python3
 """
-Unittest for class Square methods
+test_square.py - Test cases for the Square class.
 """
+
 import unittest
-from io import StringIO
-import sys
 from models.square import Square
 
+class TestSquare(unittest.TestCase):
+    def test_str(self):
+        s = Square(3, 2, 1, 5)
+        self.assertEqual(str(s), "[Square] (<5>) <2>/<1> - <3>")
 
-class Test_classSquare(unittest.TestCase):
-
-    def test_one_parameter(self):
-        """ check the new instance with one argument """
-        sq0 = Square(1)
-        self.assertEqual(sq0.size, 1)
-
-    def test_multiple_parameter(self):
-        """ check the new instance with multiple arguments """
-        sq1 = Square(1, 2)
-        self.assertEqual(sq1.width, 1)
-        self.assertEqual(sq1.height, 1)
-        self.assertEqual(sq1.x, 2)
-
-        sq2 = Square(1, 2, 3)
-        self.assertEqual(sq2.width, 1)
-        self.assertEqual(sq2.height, 1)
-        self.assertEqual(sq2.x, 2)
-        self.assertEqual(sq2.y, 3)
-
-        sq3 = Square(1, 2, 3, 4)
-        self.assertEqual(sq3.width, 1)
-        self.assertEqual(sq3.height, 1)
-        self.assertEqual(sq3.x, 2)
-        self.assertEqual(sq3.y, 3)
-        self.assertEqual(sq3.id, 4)
-
-    def test_no_int_args(self):
-        """ check if the arguments are not int values """
-        with self.assertRaises(TypeError):
-            Square("1", 2)
-        with self.assertRaises(TypeError):
-            Square(1, "2")
-        with self.assertRaises(TypeError):
-            Square(1, 2, "3")
-
-    def test_negative_size_x_y(self):
-        """ check if size, x and y are a negative int """
-        with self.assertRaises(ValueError):
-            Square(-1, 2)
-        with self.assertRaises(ValueError):
-            Square(1, -2)
-        with self.assertRaises(ValueError):
-            Square(1, 2, -3)
-
-    def test_positive_size(self):
-        """ check if size is a positive int value """
-        with self.assertRaises(ValueError):
-            Square(0)
+    def test_update(self):
+        s = Square(3, 2, 1, 5)
+        s.update(10)
+        self.assertEqual(str(s), "[Square] (<10>) <2>/<1> - <3>")
+        s.update(20, 4)
 
     def test_to_dictionary(self):
-        """ check the correct return of the to_dictionary method """
-        sq4 = Square(1, 2, 3, 4)
-        result = {'id': 4, 'size': 1, 'x': 2, 'y': 3}
-        self.assertEqual(sq4.to_dictionary(), result)
+        s = Square(3, 2, 1, 5)
+        d = s.to_dictionary()
+        expected = {
+            'id': 5,
+            'x': 2,
+            'y': 1,
+            'size': 3
+        }
+        self.assertEqual(d, expected)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
