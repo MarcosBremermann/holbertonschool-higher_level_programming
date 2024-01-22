64bbed1d3eedadc8351a43f9f69ec5bc37534a77
MarcosBremermann <mabm2004@gmail.com>
2024-01-22 19:09:52 +0000

    Task 5 v1.02: str len

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_length = len(word)
-word_first_3 = word[0:3]
+word_length = len(word); word_first_3 = word[0:3]
 word_last_2 = word[word_length - 2:word_length]
 middle_word = word[1:word_length - 1]
 print(f"First 3 letters: {word_first_3}")
