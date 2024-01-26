81ef549cbe94d0f3b545737ae60ca02791e2da01
MarcosBremermann <mabm2004@gmail.com>
2024-01-25 21:42:22 -0300

    Task 0 v1.0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 from add_0 import add
-import __main__
-import __name__
-a = 1
-b = 2
-add(a, b)
-print("{0} + {1} = {2}".format(a, b, a + b))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    add(a, b)
+    print("{0} + {1} = {2}".format(a, b, a + b))
