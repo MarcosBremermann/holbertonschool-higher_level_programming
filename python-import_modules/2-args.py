56bb541a5d33fb72710a718bcc6d64208f447a56
MarcosBremermann <mabm2004@gmail.com>
2024-01-28 12:35:07 -0300

    Task 2 v1.0

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+name = sys.argv[0]
+arguments = sys.argv[1:]
+num_arguments = len(arguments)
+
+if num_arguments == 0:
+    print("0 arguments.")
+elif num_arguments == 1:
+    print("{0} argument:".format(num_arguments))
+    print("{0}: {1}".format(num_arguments, arguments[0]))
+else:
+    print("{0} arguments:".format(num_arguments))
+    for i in range(num_arguments):
+        print("{0}: {1}".format(i+1, arguments[i]))
