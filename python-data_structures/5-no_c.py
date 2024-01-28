a25bdcd8b91202111fa3cb6ff06eea52b6358c3b
MarcosBremermann <mabm2004@gmail.com>
2024-01-28 18:19:19 -0300

    Task 5 v1.0: pycodestyle

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@ def no_c(my_string):
     new_string = ''
     for char in my_string:
         if char.lower() != 'c':
-            new_string  += char
+            new_string += char
 
     return new_string
