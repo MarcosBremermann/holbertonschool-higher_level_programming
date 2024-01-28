3644047a9847d3c5a8e31e9a36f1f729a8489623
MarcosBremermann <mabm2004@gmail.com>
2024-01-28 19:20:01 -0300

    Task 8 v1.0: pycode

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,8 +2,8 @@
 def multiple_returns(sentence):
     if not sentence:
         return (0, None)
-    
+
     first_char = sentence[0]
     length = len(sentence)
-    
+
     return (length, first_char)
