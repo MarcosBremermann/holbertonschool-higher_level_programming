4267a17e79e2ee51afe12bde722b08508c53aab4
MarcosBremermann <mabm2004@gmail.com>
2024-01-28 12:36:37 -0300

    Task 2 v1.0: added __name__ == main

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 import sys
+if __name__ == "__main__":
+    name = sys.argv[0]
+    arguments = sys.argv[1:]
+    num_arguments = len(arguments)
 
-name = sys.argv[0]
-arguments = sys.argv[1:]
-num_arguments = len(arguments)
-
-if num_arguments == 0:
-    print("0 arguments.")
-elif num_arguments == 1:
-    print("{0} argument:".format(num_arguments))
-    print("{0}: {1}".format(num_arguments, arguments[0]))
-else:
-    print("{0} arguments:".format(num_arguments))
-    for i in range(num_arguments):
-        print("{0}: {1}".format(i+1, arguments[i]))
+    if num_arguments == 0:
+        print("0 arguments.")
+    elif num_arguments == 1:
+        print("{0} argument:".format(num_arguments))
+        print("{0}: {1}".format(num_arguments, arguments[0]))
+    else:
+        print("{0} arguments:".format(num_arguments))
+        for i in range(num_arguments):
+            print("{0}: {1}".format(i+1, arguments[i]))
