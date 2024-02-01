e08df4f2eb43ea0ee73aed64f080099280d889b7
MarcosBremermann <mabm2004@gmail.com>
2024-02-01 15:44:06 -0300

    Task 2 v1.0

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    loops = 0
+    try:
+        for i in range(x):
+            try:
+                if isinstance(my_list[i], int):
+                    print("{:d}".format(my_list[i]), end="")
+                    loops += 1
+            except IndexError:
+                raise IndexError
+        print("")
+        return loops
+    except IndexError:
+        exit
