2eb68ec0d5c0722ea29e1f350976adc737df4f50
MarcosBremermann <mabm2004@gmail.com>
2024-02-04 12:43:13 -0300

    Task 3 v1.0

---
+++ python-classes/3-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+File for class Square
+"""
+
+
+class Square:
+    """
+    Square Class
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialization for priv. inst. att. size
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """
+        def that  returns the current square area
+        """
+        return self.__size * self.__size
