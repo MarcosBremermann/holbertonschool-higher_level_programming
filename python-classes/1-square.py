b3eecc21d15ccdc9c6a7e0cc4ae11ea3d3827666
MarcosBremermann <mabm2004@gmail.com>
2024-02-04 12:00:43 -0300

    Task 1 v1.0

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+File for class Square
+"""
+
+
+class Square:
+    """A class representing a square."""
+
+    def __init__(self, size):
+        """
+        Initialization for priv. inst. att. size
+        """
+        self.__size__ = size
