67342838b1855e549fa1b50b49239c05ed8e6cec
MarcosBremermann <mabm2004@gmail.com>
2024-02-11 23:50:30 -0300

    Task 3 v1.0

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+File for write file
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    function that returns the JSON
+    representation of an object (string)
+    """
+
+    return json.dumps(my_obj)
