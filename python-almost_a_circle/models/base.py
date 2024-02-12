7349d7de89584358a51ec5dae3b80c37a508021c
MarcosBremermann <mabm2004@gmail.com>
2024-02-12 16:43:15 -0300

    Task 1 v1.0

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+File for the base Class
+"""
+
+
+class Base:
+    """
+    class that will be the base of
+    all the other classes in the project.
+    Manages the id attribute.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        class constructor
+        """
+        if id is not None:
+            self.id = id
+        else:
+            __nb_objects += 1
+            self.id = __nb_objects
