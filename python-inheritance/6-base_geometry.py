6adbe98d4675483cd6958770c7437e4e9d534d84
MarcosBremermann <mabm2004@gmail.com>
2024-02-11 21:07:33 -0300

    Task 6 v1.0

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+File for def is_same_class
+"""
+
+
+class BaseGeometry:
+    """
+    class that raises an Exception
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
