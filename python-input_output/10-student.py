c4bad571fcff082d68b9e5bbe1c5c8439c205d30
MarcosBremermann <mabm2004@gmail.com>
2024-02-12 00:10:59 -0300

    Task 10 v1.0

---
+++ python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+File for write file
+"""
+
+
+class Student:
+    """
+    class that defines a student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+
+        serialized_dict = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                serialized_dict[attr] = getattr(self, attr)
+        return serialized_dict
