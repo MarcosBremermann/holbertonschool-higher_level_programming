6184a735c7bcd98250d03949832b957a1161a56b
MarcosBremermann <mabm2004@gmail.com>
2024-02-11 23:40:46 -0300

    Task 0 v1.0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -12,6 +12,6 @@ def read_file(filename=""):
     try:
         with open(filename, 'r', encoding='utf-8') as file:
             content = file.read()
-            print(content)
+            print(content, end='')
     except Exception as e:
         print(f"Error: {e}")
