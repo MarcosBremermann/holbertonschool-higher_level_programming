ca6dee61f66ea6ef2c023562aff678da6777ee9a
MarcosBremermann <mabm2004@gmail.com>
2024-02-11 23:55:00 -0300

    Task 5 v1.0

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+File for write file
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    try:
+        with open(filename, 'w') as file:
+            json.dump(my_obj, file)
+    except Exception as e:
+        print(f"Error: {e}")
