829876dcc158b4aaeb55076bfd888a782383c13d
MarcosBremermann <mabm2004@gmail.com>
2024-03-17 16:17:27 -0300

    Task 13 v1.05: pain

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,6 +4,6 @@
 SELECT tv_genres.name AS genre, COUNT(*)
 AS number_of_shows
 FROM tv_show_genres
-INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_show_genres.genre_id
 ORDER BY number_of_shows DESC, tv_genres.id ASC;
